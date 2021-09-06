bf28aeacdb6db1584be0d7559d1d3d1fbf92cff0
Shahab <syedshahab698@gmail.com>
2021-09-06 20:37:04 +0530

    Added feature to read subtitles from youtube video

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-import pandas as pd
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  6 20:07:40 2021
+
+@author: syeds
+"""
+
+from  youtube_transcript_api import YouTubeTranscriptApi
+
+url = 'https://www.youtube.com/watch?v=cIZWgPmpRVc&t=353s'
+
+url_id = url[url.find('?v=')+3:]
+
+try:
+    srt = YouTubeTranscriptApi.get_transcript(url_id)
+    print([i['text'] for i in srt])
+except:
+    print("Sorry, there are no subtitles in the video")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
